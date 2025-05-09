89b3fb74b1ac52da17ed8b30bbde003e4cbd94c7
Jertoga <bailey.haks@gmail.com>
2025-05-09 23:28:00 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -4,6 +4,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-analytics.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-storage.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-auth.js";
 
 // Firebase configuratie
 const firebaseConfig = {
@@ -37,3 +38,43 @@ function uploadImage(file) {
 
 // Exporteer de uploadImage functie om te gebruiken op je Gallery-pagina
 export { uploadImage };
+
+// Firebase Authentication
+const auth = getAuth(app);
+
+// Functie om een nieuw account aan te maken
+function signUp(email, password) {
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Account succesvol aangemaakt
+            const user = userCredential.user;
+            console.log("Account aangemaakt voor: ", user.email);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("Error tijdens registratie: ", errorCode, errorMessage);
+        });
+}
+
+// Functie om in te loggen met een bestaand account
+function signIn(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Succesvol ingelogd
+            const user = userCredential.user;
+            console.log("Ingelogd als: ", user.email);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("Error tijdens inloggen: ", errorCode, errorMessage);
+        });
+}
+
+// Functie om de inlogstatus van de gebruiker te controleren
+function onUserStateChanged(callback) {
+    onAuthStateChanged(auth, callback);
+}
+
+export { signUp, signIn, onUserStateChanged };
