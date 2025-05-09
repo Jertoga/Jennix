b888ffc03aa4f5fcbdaae4f3f3a9ef712e8a46a6
Jertoga <bailey.haks@gmail.com>
2025-05-09 23:37:42 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 function joinCommunity() {
     alert("Thanks for joining the Jenna Ortega fan community!");
 }
+
+function openSignIn() {
+    document.getElementById("signInModal").style.display = "flex";
+}
+
+function closeSignIn() {
+    document.getElementById("signInModal").style.display = "none";
+}
+
+// Close modal if clicked outside of it
+window.onclick = function(event) {
+    if (event.target === document.getElementById("signInModal")) {
+        closeSignIn();
+    }
+};
